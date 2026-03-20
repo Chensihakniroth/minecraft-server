b0db2a1b941cc3a9c9ea9d24b7d42329dcafe32e
Niroth <spam099099@gmail.com>
2026-03-20 15:50:18 +0700

    Fix: Make playit logs more visible

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Start playit agent in the background
+echo "--- STARTING PLAYIT.GG AGENT ---"
 playit &
+sleep 5 # Wait a few seconds for the claim link to generate
+echo "--- STARTING MINECRAFT SERVER ---"
 
 # Use the official entrypoint script
 exec /opt/bedrock-entry.sh
